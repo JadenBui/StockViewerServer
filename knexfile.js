ad75d6fa9650dff68632333aceba3d69e4dfc80a
JadenBui <anhbui11211@gmail.com>
2020-06-17 19:28:44 +1000

    update database instance configuration

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    client: 'mysql',
-    connection: {
-        host: '127.0.0.1',
-        port: 3307,
-        database: 'webcomputing',
-        user: 'root',
-        password: process.env.MYSQL_PASSWORD
-    }
-}
+  client: "mysql",
+  connection: {
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB,
+    user: process.env.USER,
+    password: process.env.MYSQL_PASSWORD,
+    ssl: true,
+  },
+};
